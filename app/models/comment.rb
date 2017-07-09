c8ec17b26257eda3ffeb7571cb0a33d66beb7739
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-07-09 14:48:02 +0300

    Add comment validations and posts body max length

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :user, presence: true
+  validates :post, presence: true
+  validates :body, presence: true, length: { maximum: 1500 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 40 }
+  validates :body, length: { maximum: 1500 }
   validates :user, presence: true
 end
