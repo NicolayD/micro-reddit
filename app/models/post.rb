3141546c583ff305feb36c7be886e1fefdeeddb7
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-07-09 14:53:02 +0300

    Add has_many comments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 6, maximum: 40 }
   validates :body, length: { maximum: 1500 }
   validates :user, presence: true
